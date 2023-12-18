8f5208ff7064a5284de29ec473a52ea804dcd0c9
Rafael Merino García <imrafaelmerino@gmail.com>
2023-12-18 12:30:23 +0100

    aoc 2023: day 17

--- src/main/java/advent_of_code/_2023/day17/ClumsyCrucible.java
+++ src/main/java/advent_of_code/_2023/day17/ClumsyCrucible.java
@@ -2,7 +2,6 @@ package advent_of_code._2023.day17;
 
 import advent_of_code._2023._2023_Puzzle;
 import search.BranchBoundSearch;
-import types.ControlChar;
 import types.Grid;
 import types.MutableGrid;
 import types.Pos;
@@ -11,126 +10,151 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
-
-enum Dir {N, S, E, W}
+import java.util.function.Predicate;
 
 public class ClumsyCrucible implements _2023_Puzzle {
 
 
-    // (0,0), (1,0), (2,0), (2,1), (3,1), (4,1), (5,1), (5,0), (6,0), (7,0) (8,0), (8,1), (8,2), (9,2), (10,2)
-    String a = """
-            2>>34^>>>1323
-            32v>>>35v5623
-            32552456v>>54
-            3446585845v52
-            4546657867v>6
-            14385987984v4
-            44578769877v6
-            36378779796v>
-            465496798688v
-            456467998645v
-            12246868655<v
-            25465488877v5
-            43226746555v>
-            """;
-
     @Override
     public Object solveFirst() throws Exception {
-        var input = """
-                2413432311323
-                3215453535623
-                3255245654254
-                3446585845452
-                4546657867536
-                1438598798454
-                4457876987766
-                3637877979653
-                4654967986887
-                4564679986453
-                1224686865563
-                2546548887735
-                4322674655533
-                """;
         var grid = MutableGrid.fromFile(getInputPath())
                               .mapValues((pos, val) -> Integer.parseInt(val));
 
-        var goal = new Pos(grid.xmax(),
-                           grid.ymax());
-        BiFunction<State, State, Integer> stepCost = (a, b) -> grid.getVal(b._4.pos);
+
+        BiFunction<State, State, Integer> stepCost = (a, b) -> grid.getVal(b.pos);
         var bb = BranchBoundSearch.fromStates(getSuccessor(grid),
                                               stepCost
                                              );
 
-        var start = new Pos(0, 0);
-        var pathCost = bb.findFirst(new State(null,
-                                              null,
-                                              null,
-                                              new Lava(start, Dir.E)),
-                                    s -> s._4.pos.equals(goal)
+        var goalPoint = new Pos(grid.xmax(),
+                                grid.ymax());
+        Predicate<State> goal = s -> s.pos.equals(goalPoint);
+
+        var startPoint = new Pos(0, 0);
+
+        var pathCost = bb.findFirst(new State(startPoint, Dir.E, -1),
+                                    goal
                                    );
 
-        var states = pathCost.path().states().stream().map(it -> it._4.pos).toList();
-        //grid.color(ControlChar.RED, (pos, val) -> states.contains(pos)).printRows();
+//        var states = pathCost.path().states().stream().map(it -> it.pos).toList();
+//        grid.color(ControlChar.RED, (pos, val) -> states.contains(pos)).printRows();
 
         return pathCost.cost();
     }
 
+
     private Function<State, List<State>> getSuccessor(Grid<Integer> grid) {
         return state -> {
+
             var result = new ArrayList<State>();
-            var _1 = state._1;
-            var _2 = state._2;
-            var _3 = state._3;
-            var _4 = state._4;
 
+            Pos pos = state.pos;
+            Dir dir = state.dir;
 
-            if (isSameX(_2, _3, _4)) {
-                result.add(new State(_2, _3, _4, new Lava(_4.pos.right(), Dir.E)));
-                result.add(new State(_2, _3, _4, new Lava(_4.pos.left(), Dir.W)));
-                if (_1 != null && _2.dir != _4.dir) {
-                    if (_4.dir == Dir.S) result.add(new State(_2, _3, _4, new Lava(_4.pos.up(), Dir.S)));
-                    else result.add(new State(_2, _3, _4, new Lava(_4.pos.down(), Dir.N)));
+            switch (dir) {
+                case E -> {
+                    result.add(new State(pos.right(), Dir.E, state.times + 1));
+                    result.add(new State(pos.up(), Dir.S, 1));
+                    result.add(new State(pos.down(), Dir.N, 1));
                 }
-            } else if (isSameY(_2, _3, _4)) {
-                result.add(new State(_2, _3, _4, new Lava(_4.pos.up(), Dir.S)));
-                result.add(new State(_2, _3, _4, new Lava(_4.pos.down(), Dir.N)));
-                if (_1 != null && _2.dir != _4.dir) {
-                    if (_4.dir == Dir.E) result.add(new State(_2, _3, _4, new Lava(_4.pos.right(), Dir.E)));
-                    else result.add(new State(_2, _3, _4, new Lava(_4.pos.left(), Dir.W)));
+                case W -> {
+                    result.add(new State(pos.left(), Dir.W, state.times + 1));
+                    result.add(new State(pos.up(), Dir.S, 1));
+                    result.add(new State(pos.down(), Dir.N, 1));
                 }
-            } else {
-                result.add(new State(_2, _3, _4, new Lava(_4.pos.right(), Dir.E)));
-                result.add(new State(_2, _3, _4, new Lava(_4.pos.left(), Dir.W)));
-                result.add(new State(_2, _3, _4, new Lava(_4.pos.up(), Dir.S)));
-                result.add(new State(_2, _3, _4, new Lava(_4.pos.down(), Dir.N)));
+                case N -> {
+                    result.add(new State(pos.down(), Dir.N, state.times + 1));
+                    result.add(new State(pos.left(), Dir.W, 1));
+                    result.add(new State(pos.right(), Dir.E, 1));
+                }
+                case S -> {
+                    result.add(new State(pos.up(), Dir.S, state.times + 1));
+                    result.add(new State(pos.left(), Dir.W, 1));
+                    result.add(new State(pos.right(), Dir.E, 1));
+                }
             }
 
+            return result.stream().filter(s -> grid.containsPos(s.pos) && s.times <= 3).toList();
 
-            return result.stream()
-                         .filter(it ->
-                                         grid.containsPos(it._4.pos)
-                                       && (_2 == null || !_4.pos.equals(_2.pos)))
-                         .toList();
+
         };
 
 
     }
 
-    private boolean isSameX(Lava _1, Lava _2, Lava _3) {
-        return _1 != null && _2 != null && _3 != null
-               && _1.pos.x() == _2.pos.x()
-               && _2.pos.x() == _3.pos.x();
+    private Function<State, List<State>> getSuccessor2(Grid<Integer> grid) {
+        return state -> {
+
+            var result = new ArrayList<State>();
+
+            Pos pos = state.pos;
+            Dir dir = state.dir;
+
+            switch (dir) {
+                case E -> {
+                    result.add(new State(pos.right(), Dir.E, state.times + 1));
+                    if (state.times >= 4) {
+                        result.add(new State(pos.up(), Dir.S, 1));
+                        result.add(new State(pos.down(), Dir.N, 1));
+                    }
+                }
+                case W -> {
+                    result.add(new State(pos.left(), Dir.W, state.times + 1));
+                    if (state.times >= 4) {
+                        result.add(new State(pos.up(), Dir.S, 1));
+                        result.add(new State(pos.down(), Dir.N, 1));
+                    }
+                }
+                case N -> {
+                    result.add(new State(pos.down(), Dir.N, state.times + 1));
+                    if (state.times >= 4) {
+                        result.add(new State(pos.left(), Dir.W, 1));
+                        result.add(new State(pos.right(), Dir.E, 1));
+                    }
+                }
+                case S -> {
+                    result.add(new State(pos.up(), Dir.S, state.times + 1));
+                    if (state.times >= 4) {
+                        result.add(new State(pos.left(), Dir.W, 1));
+                        result.add(new State(pos.right(), Dir.E, 1));
+                    }
+                }
+            }
+
+            return result.stream()
+                         .filter(s -> grid.containsPos(s.pos) && s.times <= 10)
+                         .toList();
+
+
+        };
+
+
     }
 
-    private boolean isSameY(Lava _1, Lava _2, Lava _3) {
-        return _1 != null && _2 != null && _3 != null
-               && _1.pos.y() == _2.pos.y()
-               && _2.pos.y() == _3.pos.y();
-    }
 
     @Override
     public Object solveSecond() throws Exception {
-        return null;
+        var grid = MutableGrid.fromFile(getInputPath())
+                              .mapValues((pos, val) -> Integer.parseInt(val));
+
+
+        BiFunction<State, State, Integer> stepCost = (a, b) -> grid.getVal(b.pos);
+        var bb = BranchBoundSearch.fromStates(getSuccessor2(grid),
+                                              stepCost
+                                             );
+        var goalPoint = new Pos(grid.xmax(),
+                                grid.ymax());
+        Predicate<State> goal = s -> s.pos.equals(goalPoint) && s.times >= 4;
+
+        var start = new Pos(0, 0);
+        var pathCost = bb.findFirst(new State(start, Dir.S, -1),
+                                    goal
+                                   );
+
+//        var states = pathCost.path().states().stream().map(it -> it.pos).toList();
+//        grid.color(ControlChar.RED, (pos, val) -> states.contains(pos)).printRows();
+
+        return pathCost.cost();
     }
 
     @Override
@@ -150,17 +174,17 @@ public class ClumsyCrucible implements _2023_Puzzle {
 
     @Override
     public String outputUnitsPart2() {
-        return null;
+        return outputUnitsPart1();
     }
+
+    enum Dir {N, S, E, W}
 
     /**
-     * we need to distinguish  v>>> from >>>> for that we'll use the
+     * @param pos   current position
+     * @param dir   last move direction
+     * @param times number moves in the same direction (reset to 0 when changing directions)
      */
-
-    record Lava(Pos pos, Dir dir) {
-    }
-
-    record State(Lava _1, Lava _2, Lava _3, Lava _4) {
+    record State(Pos pos, Dir dir, int times) {
 
 
     }
